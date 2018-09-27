b28a86436af5ee568a9717a25cf152e6e30d61ee
S.P. Mohanty <spmohanty91@gmail.com>
2018-09-27 19:45:49 +0200

    Update run.sj

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [[ -z "${CROWDAI_IS_GRADING}" ]]; then
+if [[ -z "${CROWDAI_MARLO_GRADING}" ]]; then
 	source activate marlo #Use This line would work in case of local debugs;
 	# in a local debug setup, you should use your env name instead of marlo
+	echo "marlo"
 else
 	source activate root #Use This line would work in case of the docker container
+	echo "root"
 fi
 
 python run.py
