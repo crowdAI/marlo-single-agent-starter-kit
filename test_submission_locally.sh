d48cb3ff5f3b9317492b5f23ecf9cb7d9722e3cd
SP Mohanty <spmohanty91@gmail.com>
2018-09-27 15:40:23 +0200

    test_submission_locally

---
+++ test_submission_locally.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# Build Image from the repository 
+./build.sh
+
+# Ensure you have a Minecraft Client running on port 10000
+# by doing : $MALMO_MINECRAFT_ROOT/launchClient.sh -port 10000
+
+docker run --net=host -it $IMAGE_NAME /home/crowdai/run.sh
+
+# Now if everything works out well, then you should see the agent inside 
+# the docker container interacting with the minecraft client on your host.
