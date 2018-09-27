97b2eadf3209bd31de89632687545c53a6593d52
S.P. Mohanty <spmohanty91@gmail.com>
2018-09-27 19:35:44 +0200

    Add explanatory comments

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ -z "${CROWDAI_IS_GRADING}" ]]; then
-	source activate marlo #Use This line would work in case of local debugs
+	source activate marlo #Use This line would work in case of local debugs;
+	# in a local debug setup, you should use your env name instead of marlo
 else
 	source activate root #Use This line would work in case of the docker container
 fi
